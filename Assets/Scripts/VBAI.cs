b5409d9fb7dbaa1b3cd3449bc3b225c5f17fdc2b
Bimantara Putra Ernandra <bima.pe02@gmail.com>
2025-11-12 11:57:48 +0700

    VB Anim WIP

--- Assets/Scripts/VBAI.cs
+++ Assets/Scripts/VBAI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))] // Memastikan objek ini punya Rigidbody2D
-public class EnemyAI : MonoBehaviour
+public class VBAI : MonoBehaviour
 {
     public float speed = 2f;
     private Transform player;
